ad6d93659969c0e9d623ec95e424f51538cd7be8
itgirlarchive <151342467+itgirlarchive@users.noreply.github.com>
2025-10-26 20:50:45 +0000

    Added features to allow users to add as many notes at once. Includes different oscillations of sine waves to match the sounds accordingly in the order the user submits. Sound starts playing right away.

--- main.js
+++ main.js
@@ -4,16 +4,49 @@ const input = document.getElementById('input');
 var amplitude = 40; 
 var interval = null; 
 var pitch = 0; 
+var freq = 0; 
 var x = 0;  
 var y = 0; 
-var freq = 0; 
 var counter = 0; 
+var reset = false; 
+var timepernote = 0; 
+var length = 0; 
 
 //define canvas variables 
 var canvas = document.getElementById("canvas"); 
 var ctx = canvas.getContext("2d"); 
 var width = ctx.canvas.width; 
 var height = ctx.canvas.height; 
+
+
+function drawWave() {
+    clearInterval(interval); 
+    counter = 0; 
+    if (reset) {
+        ctx.clearRect(0, 0, width, height); 
+        x = 0; 
+        y = (height/2);   
+        
+        ctx.moveTo(x, y);
+        ctx.beginPath();
+    }
+    
+    interval = setInterval(line,20); 
+    reset = false; 
+} 
+
+function line() {
+    //increase counter by 1 to show how long interval has been run 
+    counter++; 
+    freq = pitch/10000; 
+    y = (height/2) + amplitude * Math.sin(2 * (Math.PI) * freq * x * (0.5 * length)); 
+    ctx.lineTo(x,y); 
+    ctx.stroke(); 
+    x = x + 1; 
+    if (counter > (timepernote/20)) {
+        clearInterval(interval); 
+    }
+}
 
 //create web audio api elements
 const audioCtx = new AudioContext(); 
@@ -37,42 +70,44 @@ noteNames.set("G", 392.0);
 noteNames.set("A", 440);
 noteNames.set("B", 493.9);
 
-function frequency(pitch) {
-gainNode.gain.setValueAtTime(100,audioCtx.currentTime); 
-oscillator.frequency.setValueAtTime(pitch,audioCtx.currentTime); 
-gainNode.gain.setValueAtTime(0,audioCtx.currentTime + 1); 
+function frequency(pitchValue) {
+    pitch = pitchValue
+    gainNode.gain.setValueAtTime(100,audioCtx.currentTime); 
+    oscillator.frequency.setValueAtTime(pitch,audioCtx.currentTime); 
+    gainNode.gain.setValueAtTime(0,audioCtx.currentTime + (timepernote/1000)-0.1); 
 }
 
 function handle(){
+    reset = true; 
     audioCtx.resume(); 
+    gainNode.gain.value = 0; 
     var usernotes = String(input.value); 
-    pitch = noteNames.get(usernotes); 
-    frequency(pitch); 
-    drawWave(); 
+    var noteslist = []; 
+    length = usernotes.length
+    timepernote = (6000 / length); 
+
+    for (i = 0; i < usernotes.length; i++) {
+        noteslist.push(noteNames.get(usernotes.charAt(i))); 
     }
 
-function drawWave() {
-    
-        ctx.clearRect(0, 0, width, height); 
-        x = 0; 
-        y = height/2; 
-        ctx.beginPath(); 
-        ctx.moveTo(x,y); 
-    
-    counter = 0; 
-    interval = setInterval(line,20); 
-} 
-
-function line() {
-    //increase counter by 1 to show how long interval has been run 
-    counter++; 
-    freq = pitch / 10000; 
-    y = height/2 + (amplitude * Math.sin(x * 2 * Math.PI * freq)); 
-    ctx.lineTo(x,y); 
-    ctx.stroke(); 
-    x = x + 1; 
-    if (counter > 50) {
-        clearInterval(interval); 
+    let j = 0; 
+    //Advanced challenge: playing the first note immediately 
+    if (noteslist.length >0) {
+        frequency(noteslist[j]); 
+        drawWave(); 
+        j++; 
     }
-}
+    //continue with rest 
+    repeat = setInterval(() => {
+        if (j < noteslist.length) {
+            frequency(parseInt(noteslist[j])); 
+            drawWave(); 
+        j++ 
+        } else {
+            clearInterval(repeat)
+        }
+    }, timepernote) 
+    }
+
+
 
