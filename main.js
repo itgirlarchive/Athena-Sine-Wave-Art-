c79d1b6bb5465f52c6969fd530e3cbd9b5eaab7c
itgirlarchive <151342467+itgirlarchive@users.noreply.github.com>
2025-10-27 00:28:11 +0000

    Features to record, change line thickness, and piano mode!!! You can also pause with the space bar now for better rhythm. Also minor details to center site /add new font.

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const input = document.getElementById('input');
+const recording_toggle = document.getElementById('record'); 
+const thickness_slider = document.getElementById('thickness-slider'); 
 
 //INITIALIZING VARIABLES 
 var interval = null; 
@@ -10,6 +12,21 @@ var counter = 0;
 var reset = false; 
 var timepernote = 0; 
 var length = 0; 
+var pianoMode = false; 
+
+function togglePianoMode() {
+    pianoMode = !pianoMode; 
+    const button = document.getElementById('piano-mode'); 
+
+    if (pianoMode){
+        oscillator.type = "triangle"; 
+        button.innerHTML = "Piano Mode: ON"; 
+    } else {
+        oscillator.type = "sine"; 
+        button.innerHTML = "Piano Mode: OFF"; 
+    }
+}
+
 
 //DEFINE CANVAS VARIABLES 
 var canvas = document.getElementById("canvas"); 
@@ -59,6 +76,8 @@ function interpolateColor(color1, color2, factor) {
 function line() {
     //increase counter by 1 to show how long interval has been run 
     counter++; 
+    ctx.lineWidth = thickness_slider.value; 
+
     freq = pitch/10000; 
     y = (height/2) + ((vol_slider.value/100)*40) * Math.sin(2 * (Math.PI) * freq * x * (0.5 * length)); 
     
@@ -93,15 +112,18 @@ gainNode.gain.value = 0;
 
 //DECLARES NEW MAP: SHORTHAND FOR FREQUENCIES 
 noteNames = new Map(); 
-noteNames.set("C", 261.6);
-noteNames.set("D", 293.7);
-noteNames.set("E", 329.6);
-noteNames.set("F", 349.2);
+noteNames.set("C", 261.63);
+noteNames.set("D", 293.66);
+noteNames.set("E", 329.63);
+noteNames.set("F", 349.23);
+// S is F sharp 
+noteNames.set("S",369.99);
 noteNames.set("G", 392.0);
 noteNames.set("A", 440);
-noteNames.set("B", 493.9);
+noteNames.set("B", 493.88);
 
 //FREQUENCY OF PITCH 
+/*
 function frequency(pitchValue) {
     pitch = pitchValue
     gainNode.gain.setValueAtTime(vol_slider.value,audioCtx.currentTime); 
@@ -112,7 +134,35 @@ function frequency(pitchValue) {
     gainNode.gain.value = 0; }, ((timepernote) -10)); 
     
 }
+*/
 
+function frequency(pitchValue){
+    pitch = pitchValue; 
+    const now = audioCtx.currentTime; 
+    const noteDuration = (timepernote/1000) - 0.1; 
+
+    if (pianoMode) {
+        gainNode.gain.cancelScheduledValues(now); 
+        gainNode.gain.setValueAtTime(0, now); 
+        gainNode.gain.linearRampToValueAtTime(vol_slider.value, now+0.005);
+        gainNode.gain.exponentialRampToValueAtTime(Math.max(vol_slider.value * 0.1, 0.01), now + 0.2);
+        gainNode.gain.linearRampToValueAtTime(0.01,now+noteDuration); 
+
+        
+    } else {
+        gainNode.gain.setValueAtTime(vol_slider.value, now); 
+        setting = setInterval(() => {gainNode.gain.value = vol_slider.value}, 1); 
+        setTimeout(() => {
+            clearInterval(setting); 
+            gainNode.gain.value = 0; 
+
+    }, ((timepernote)- 10));  
+    
+    }
+oscillator.frequency.setValueAtTime(pitchValue, now); 
+}
+
+/*
 function handle(){
     reset = true; 
     audioCtx.resume(); 
@@ -144,6 +194,100 @@ function handle(){
         }
     }, timepernote) 
     }
+*/
+
+function handle(){
+    reset = true; 
+    audioCtx.resume(); 
+    gainNode.gain.value = 0; 
+    var usernotes = String(input.value); 
+    var noteslist = []; 
+    length = usernotes.length
+    timepernote = (6000 / length); 
+
+    for (i = 0; i < usernotes.length; i++) {
+        const char = usernotes.charAt(i); 
+        if (char === ' ') {
+            noteslist.push(null); 
+        } else {
+            noteslist.push(noteNames.get(usernotes.charAt(i))); 
+        }
+    }
+
+    let j = 0; 
+    //Advanced challenge: playing the first note immediately  
+    if (noteslist.length > 0 && noteslist[0] !== null) {
+        frequency(noteslist[j]); 
+        drawWave(); 
+        j++; 
+    } else if (noteslist[0] === null) {
+        j++; 
+    }
+
+    function checkAndPlay(){
+        if (j < noteslist.length) {
+            if(noteslist[j] !== null){
+                frequency(noteslist[j]); 
+                drawWave();
+                j++; 
+            } else {
+                clearInterval(repeat); 
+                setTimeout(() => {
+                    j++;
+                    repeat = setInterval(checkAndPlay, timepernote); 
+                }, 0.01); 
+            }
+            } else {
+                clearInterval(repeat);
+            }
+        }
+
+        repeat = setInterval(checkAndPlay, timepernote); 
+
+    }
+    
 
 
+var blob, recorder = null; 
+var chunks = []; 
 
+function startRecording(){
+    const canvasStream = canvas.captureStream(20); 
+    const audioDestination = audioCtx.createMediaStreamDestination(); 
+    gainNode.connect(audioDestination); 
+    const combinedStream = new MediaStream(); 
+    canvasStream.getVideoTracks().forEach(track => combinedStream.addTrack(track)); 
+    audioDestination.stream.getAudioTracks().forEach(track => combinedStream.addTrack(track));
+    recorder = new MediaRecorder(combinedStream, { mimeType: 'video/webm'}); 
+    
+    recorder.ondataavailable = e => {
+    if (e.data.size > 0) {
+        chunks.push(e.data);
+    }
+    };
+
+
+    recorder.onstop = () => {
+        const blob = new Blob(chunks, { type: 'video/webm' });
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = 'recording.webm';
+        a.click();
+        URL.revokeObjectURL(url);
+    }
+
+    recorder.start();
+}
+
+var is_recording = false; 
+function toggle(){
+    is_recording = !is_recording; 
+    if(is_recording){
+        recording_toggle.innerHTML = "Stop Recording";
+        startRecording(); 
+    } else {
+        recording_toggle.innerHTML = "Start Recording"; 
+        recorder.stop(); 
+    }
+}
